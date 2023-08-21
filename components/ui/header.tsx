814f43d0204b7897d2cd6b77ed075dc99dcabc01
Mariam Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2023-08-21 19:53:23 +0300

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -7,15 +7,24 @@ export default function Header() {
     <header className="fixed top-0 right-0 left-0 h-auto bg-gray-900 w-full z-30">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-20">
-
-          {/* Site branding */}
-          <div className="flex grow shrink-0 justify-center items-center">
-            {/* Logo */}
-
-            <Link href='/' className='hidden md:flex md:items-center md:gap-4'>
-              <Image src='/images/logo.png' alt='logo' width={50} height={50} />
-              <p className='text-2xl font-bold text-white'>Mō K!</p>
-             </Link>
+          <div className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-center flex-wrap items-center">
+              <li>
+                <Link
+                  href="/signin"
+                  className="text-lg text-white hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  تسجيل الدخول
+                </Link>
+              </li>
+              
+              <li>
+                <Link href="/signup" className="btn-sm text-lg text-white bg-purple-600 hover:bg-gray-900 ml-3">
+                  اشتراك
+                </Link>
+              </li>
+            </ul>
           </div>
 
           {/* Desktop navigation */}
@@ -61,24 +70,14 @@ export default function Header() {
             </ul>
           </div>
 
-          <div className="hidden md:flex md:grow">
-            {/* Desktop sign in links */}
-            <ul className="flex grow justify-center flex-wrap items-center">
-              <li>
-                <Link
-                  href="/signin"
-                  className="text-lg text-white hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  تسجيل الدخول
-                </Link>
-              </li>
-              
-              <li>
-                <Link href="/signup" className="btn-sm text-lg text-white bg-purple-600 hover:bg-gray-900 ml-3">
-                  اشتراك
-                </Link>
-              </li>
-            </ul>
+          {/* Site branding */}
+          <div className="flex grow shrink-0 justify-center items-center">
+            {/* Logo */}
+
+            <Link href='/' className='hidden md:flex md:items-center md:gap-4'>
+              <Image src='/images/logo.png' alt='logo' width={50} height={50} />
+              <p className='text-2xl font-bold text-white'>Mō K!</p>
+             </Link>
           </div>
 
           <MobileMenu />
